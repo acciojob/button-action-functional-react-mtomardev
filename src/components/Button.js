554613e64acdd73c1705446f593308dcfe5858aa
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-04-29 19:23:00 +0000

    second

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,19 @@ import React, {useState} from "react";
 
 const Button = () =>{
     let [paravalue , setParavalue] = useState("")
-    
+    const [showParagraph, setShowParagraph] = useState(false);
+
     function handlepara(){
+        setShowParagraph(true)
         setParavalue("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.")
     }
     return(
         <div>
-            <button id="click" onClick={handlepara}></button>
-            <p id="para">{paravalue}</p>
+            <button id="click" onClick={handlepara}>Click</button>
+            {
+                showParagraph && <p id="para">{paravalue}</p> 
+            }
+            
         </div>
     )
 }
